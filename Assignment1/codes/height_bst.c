8bee10030c6fe0781ffcc1d280f8556378cf9fd0
Krati012 <47531726+Krati012@users.noreply.github.com>
2021-10-12 01:21:23 +0530

    Update height_bst.c

--- Assignment1/codes/height_bst.c
+++ Assignment1/codes/height_bst.c
@@ -32,7 +32,7 @@ struct Node* construct(int in[], int post[], int inStrt, int inEnd, int* pIndex)
         return NULL;
 
     //Pick current node from post[] using postIndex and decrement postIndex
-    struct Node* node = newNode(post[*pIndex]);
+    struct Node* node = newNode(*(post + *pIndex));
     (*pIndex)--;
 
     //If this node has no children, return node
@@ -71,9 +71,15 @@ int maxDepth(struct Node* node){
         return rDepth+1;
 }
 
-//Helper function for generate_inorder()
-void printInorder(struct Node* node, int *array)
-{
+//Swap function to swap variable values pointed by xp and yp
+void swap(int *xp, int *yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+//Helper function for inorder array for generate_order()
+void printInorder(struct Node* node, int *array){
 	static int count = 0;
     if (node == NULL)
         return;
@@ -87,9 +93,24 @@ void printInorder(struct Node* node, int *array)
     return;
 }
 
-//Generates inorder array for any height 
-int *generate_inorder(int height)
-{
+//Helper function for postorder array for generate_order()
+void printPostorder(struct Node* node, int *array){
+    static int count = 0;
+    if (node == NULL)
+        return;
+
+    printPostorder(node->left, array);
+    printPostorder(node->right,array);
+
+ 	array[count] = node->data;
+ 	count++;
+
+    return;
+}
+
+//Generates traversal array for any height
+//Use iop = 1 for inorder, iop = 2 for postorder
+int *generate_order(int height, int iop){
 	//allocate space as for ht h, number of elements in array : 2*ht + 1
 	int *array = (int *)malloc((2*height + 1)*sizeof(int));
 
@@ -109,52 +130,42 @@ int *generate_inorder(int height)
 	}
 	temp->left = NULL;
 	temp->right = NULL;
-	//Puts element of the tree into an array in inorder traversal
-	printInorder(root, array);
+	//Puts element of the tree into an array
+    if(iop == 1){
+        printInorder(root, array);      //iop = 1 for inorder traversal
+    }
+	else{
+        printPostorder(root, array);    //iop = 2 for postorder traversal
+	}
 	//returns pointer to the array
 	return array;
 }
 
 int main(){
-    int in[] = {8,6,9,4,7,2,5,1,3};
-	int post[] = {8,9,6,7,4,5,2,3,1};
-    int n = sizeof(in)/sizeof(in[0]);
+    //Using array given in question
+    int in1[] = {8,6,9,4,7,2,5,1,3};
+	int post1[] = {8,9,6,7,4,5,2,3,1};
+    int n1 = sizeof(in1)/sizeof(in1[0]);
 
-    struct Node* root = constructTree(in, post, n);
+    struct Node* root1 = constructTree(in1, post1, n1);
 
-    int ht = maxDepth(root) - 1;
+    int ht1 = maxDepth(root1) - 1;
+    printf("Height of Binary Tree is %d\n", ht1);
 
-    printf("Height of Binary Tree is %d\n", ht);
-
-    //Verification using random binary tree of height 20
-    int in2[] = {40, 38, 41, 36, 39, 34, 37, 32, 35, 30, 33, 28, 31, 26, 29, 24,
-            27, 22, 25, 20, 23, 18, 21, 16, 19, 14, 17, 12, 15, 10, 13, 8, 11,
-            6, 9, 4, 7, 2, 5, 1, 3};
-    int post2[] = {40, 41, 38, 39, 36, 37, 34, 35, 32, 33, 30, 31, 28, 29, 26, 27, 24,
-            25, 22, 23, 20, 21, 18, 19, 16, 17, 14, 15, 12, 13, 10, 11, 8, 9, 6,
-            7, 4, 5, 2, 3, 1};
-
-    int m = sizeof(in2)/sizeof(in2[0]);
-    struct Node* root2 = constructTree(in2, post2, m);
-
-    int ht2 = maxDepth(root2) - 1;
-    printf("Height of Binary Tree is %d\n", ht2);
-
-    int h = 20;
-    //scanf("Enter required height %d", h);
+    //Verification by generating random binary tree
+    int h ;
+    printf("Enter required height: ");
+    scanf("%d", &h);
     int x = 2*h+ 1;
-    int *p = generate_inorder(h);
+    int *in2 = generate_order(h, 1);
+    int *post2 = generate_order(h, 2);
 
-    printf("Inorder array for height %d is: ", h);
-    for(int i=0; i<x; i++){
-        printf("%d ", *(p + i));
+    //Using generated inorder & postorder array to find height of binary tree
+    struct Node* root2 = constructTree(in2, post2, x);
+    int ht2 = maxDepth(root2) - 1;
+    printf("\nHeight of Generated Binary Tree is %d\n", ht2);
+    if(h == ht2){
+        printf("Algorithm verified\n");
     }
-
-    //Using generated inorder array & prev postorder to find height(20) of binary tree
-    struct Node* root3 = constructTree(p, post2, m);
-
-    int ht3 = maxDepth(root3) - 1;
-    printf("\n Height of Binary Tree is %d\n", ht3);
-
     return 0;
 }
